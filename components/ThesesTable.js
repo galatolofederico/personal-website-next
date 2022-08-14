20129717d6ccb8e4982070497a9d596005eec032
Federico Galatolo <galatolo.federico@gmail.com>
2022-08-14 16:49:41 +0200

    added ThesesTable components

---
+++ components/ThesesTable.js
@@ -0,0 +1,23 @@
+import { Table, Text, Badge, Group } from '@mantine/core';
+
+export const ThesesTable = ({ theses }) => {
+    theses.sort((a, b) =>
+        (new Date(b.date.year, b.date.month, b.date.day))
+        -
+        (new Date(a.date.year, a.date.month, a.date.day))
+    )
+    const rows = theses.map((thesis) => {
+        return <>
+            <tr key={thesis.name}>
+                <td><Text weight={700}>{thesis.title} <Badge>{thesis.type}</Badge></Text></td>
+                <td>{thesis.author}</td>
+            </tr>
+        </>
+    })
+
+    return (
+        <Table fontSize="md" highlightOnHover>
+            <tbody>{rows}</tbody>
+        </Table>
+    )
+}
---
+++ stories/ThesesTable.stories.js
@@ -0,0 +1,15 @@
+import { Accordion, Grid } from '@mantine/core';
+import { ThesesTable } from '../components/ThesesTable';
+
+import * as mejson from "./data/me.json"
+
+export default {
+  title: 'Components/ThesesTable',
+  component: ThesesTable,
+}
+
+export const Default = () => <>
+  <Grid justify="center" align="center">
+    <Grid.Col md={8} sm={12}><ThesesTable theses={mejson.advised_theses} /></Grid.Col>
+  </Grid>
+</>
