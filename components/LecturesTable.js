cf54e15f97c2b9f090d844890b27f5e719f02ec7
Federico Galatolo <galatolo.federico@gmail.com>
2022-08-14 15:57:44 +0200

    hide position column in LecturesTable if there is no space

--- components/LecturesTable.js
+++ components/LecturesTable.js
@@ -1,7 +1,19 @@
-import { Button, Group, Table, Text } from '@mantine/core';
+import { Button, Group, Stack, Table, Text } from '@mantine/core';
+import React, { useState, useEffect } from 'react';
 
-export const LecturesTable = ({lectures}) => {
-    lectures.sort((a, b) => 
+export const LecturesTable = ({ lectures }) => {
+    const [bigScreen, setBigScreen] = useState(
+        window.matchMedia("(min-width: 768px)").matches
+    )
+
+    useEffect(() => {
+        window
+            .matchMedia("(min-width: 768px)")
+            .addEventListener('change', e => setBigScreen(e.matches));
+    }, []);
+
+
+    lectures.sort((a, b) =>
         (new Date(b.date.year, b.date.month, b.date.day))
         -
         (new Date(a.date.year, a.date.month, a.date.day))
@@ -10,8 +22,16 @@ export const LecturesTable = ({lectures}) => {
         const buttons = lecture.resources.map(r => (<Button size="xs" compact onClick={() => window.open(r.link)}>{r.name}</Button>))
         return <>
             <tr key={lecture.name}>
-                <td>{lecture.title}</td>
-                <td>{lecture.container} - <Text color="dimmed">{lecture.position}</Text></td>
+                <td><Text weight={700}>{lecture.title}</Text></td>
+                {bigScreen ?
+                <td>
+                    <Stack spacing={0}>
+                        <Text size="sm">{lecture.container}</Text>
+                        <Text size="sm" color="dimmed">{lecture.position}</Text>
+                    </Stack>
+                </td>
+                : <></>
+                }
                 <td><Group >{buttons}</Group></td>
             </tr>
         </>
