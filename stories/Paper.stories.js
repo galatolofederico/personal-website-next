cdc9185ba83627993c18d06c0c0af2ecacf7fc12
Federico Galatolo <galatolo.federico@gmail.com>
2022-09-30 17:52:30 +0200

    better stories for paper

--- stories/Paper.stories.js
+++ stories/Paper.stories.js
@@ -8,25 +8,59 @@ export default {
   component: Paper,
 }
 
-export const Conference_Published = () => <>
-  <Grid justify="center" align="center">
-    <Grid.Col md={8} sm={12}>
-        <Paper {...mejson.publications[mejson.publications.length-1]} />
-    </Grid.Col>
-  </Grid>
-</>
+export const Published_NoCode_NoData = () => {
+  let pub = mejson.publications[Math.floor(Math.random()*mejson.publications.length)]
+  pub.doi = "dummy-doi"
+  pub.code = ""
+  pub.data = ""
+  return <>
+    <Grid justify="center" align="center">
+      <Grid.Col md={8} sm={12}>
+          <Paper {...pub} />
+      </Grid.Col>
+    </Grid>
+  </>
+}
 
-export const Journal_Published = () => <>
-  <Grid justify="center" align="center">
-    <Grid.Col md={8} sm={12}>
-        <Paper {...mejson.publications[mejson.publications.length-6]} />
-    </Grid.Col>
-  </Grid>
-</>
-export const Journal_Unpublished = () => <>
-<Grid justify="center" align="center">
-  <Grid.Col md={8} sm={12}>
-      <Paper {...mejson.publications[mejson.publications.length-10]} />
-  </Grid.Col>
-</Grid>
-</>
+export const Unpublished_NoCode_NoData = () => {
+  let pub = mejson.publications[Math.floor(Math.random()*mejson.publications.length)]
+  pub.doi = ""
+  pub.code = ""
+  pub.data = ""
+  return <>
+    <Grid justify="center" align="center">
+      <Grid.Col md={8} sm={12}>
+          <Paper {...pub} />
+      </Grid.Col>
+    </Grid>
+  </>
+}
+
+
+export const Published_Code_Data = () => {
+  let pub = mejson.publications[Math.floor(Math.random()*mejson.publications.length)]
+  pub.doi = "dummy-doi"
+  pub.code = "dummy-code"
+  pub.data = "dummy-data"
+  return <>
+    <Grid justify="center" align="center">
+      <Grid.Col md={8} sm={12}>
+          <Paper {...pub} />
+      </Grid.Col>
+    </Grid>
+  </>
+}
+
+export const Unpublished_Code_Data = () => {
+  let pub = mejson.publications[Math.floor(Math.random()*mejson.publications.length)]
+  pub.doi = ""
+  pub.code = "dummy-code"
+  pub.data = "dummy-data"
+  return <>
+    <Grid justify="center" align="center">
+      <Grid.Col md={8} sm={12}>
+          <Paper {...pub} />
+      </Grid.Col>
+    </Grid>
+  </>
+}
