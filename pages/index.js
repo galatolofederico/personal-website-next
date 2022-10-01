a24ef3894ab399900253c2f3643a7e368aee6ae9
Federico Galatolo <galatolo.federico@gmail.com>
2022-10-01 15:40:54 +0200

    home page work in progress

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,70 @@
-import { Loader } from '@mantine/core'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import { Grid, Stack, Center, Image, Text, Title } from '@mantine/core';
 import { Navbar } from '../components/Navbar';
+import fsPromises from 'fs/promises';
+import path from 'path';
 
-export default function Home() {
+
+function Picture({anagraphic}){
   return <>
-    <Navbar />
+    <Grid justify="center" align="center">
+      <Grid.Col span={6} xs={4} sm={4} md={3} lg={2} xl={2}>
+        <Image
+          radius={1000}
+          src={anagraphic.picture}
+          alt="Random unsplash image"
+        />
+      </Grid.Col>
+    </Grid>
   </>
+}
+
+function Name({anagraphic}){
+  return <>
+    <Stack spacing={0}>
+      <Center>
+        <Text style={{fontSize: 35, lineHeight: "1em"}}>
+          {anagraphic.fullname.first} {anagraphic.fullname.middle[0]}. {anagraphic.fullname.last}
+        </Text>
+      </Center>
+      <Center>
+        <Text style={{fontSize: 20}}>
+          {anagraphic.qualifications[0]}
+        </Text>
+      </Center>
+    </Stack>
+  </>
+}
+
+
+function Home({anagraphic, digitalidentity, publications, projects, lectures}){
+  return <>
+  <Stack>
+    <Picture anagraphic={anagraphic}/>
+    <Name anagraphic={anagraphic} />
+  </Stack>
+</>
+}
+
+export default function HomePage(props) {
+  return <>
+    <Stack>
+      <Navbar />
+      <Home {...props}/>
+    </Stack>
+  </>
+}
+
+export async function getStaticProps() {
+  const mejson_path = path.join(process.cwd(), "./me.json/me.json")
+  const mejson = JSON.parse(await fsPromises.readFile(mejson_path))
+
+  return {
+    props: {
+      anagraphic: mejson.anagraphic,
+      digitalidentity: mejson.digitalidentity,
+      publications: mejson.publications,
+      projects: mejson.projects,
+      lectures: mejson.lectures
+    }
+  }
 }
